9af20d0a23c8578103601e3e7dc30b92b467a992
Garrett Gregor <garrett.gregor@gmail.com>
2023-12-03 04:24:53 -0500

    Test: add #power_per_game

--- day_2/spec/conundrum_spec.rb
+++ day_2/spec/conundrum_spec.rb
@@ -48,4 +48,23 @@ RSpec.describe Conundrum do
       expect(conundrum.min_per_game(e)).to eq({"blue" => 2, "red" => 6, "green" => 3})
     end
   end
+
+  describe "power_per_game" do
+    it "gives a minimum amount of cubes per color that was required per game" do
+      file = File.open("./fixtures/input.txt")
+      conundrum = Conundrum.new(file)
+
+      a = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+      b = "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"
+      c = "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
+      d = "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"
+      e = "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+
+      expect(conundrum.min_per_game(a)).to eq(48)
+      expect(conundrum.min_per_game(b)).to eq(12)
+      expect(conundrum.min_per_game(c)).to eq(1560)
+      expect(conundrum.min_per_game(d)).to eq(630)
+      expect(conundrum.min_per_game(e)).to eq(36)
+    end
+  end
 end
