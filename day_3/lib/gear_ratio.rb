ed8773a5773d576d98b738c9008dcef89487ad7d
Garrett Gregor <garrett.gregor@gmail.com>
2023-12-04 07:55:32 +0200

    add comments for  parsing numbers

--- day_3/lib/gear_ratio.rb
+++ day_3/lib/gear_ratio.rb
@@ -8,6 +8,11 @@ class GearRatio
     @symbol_coordinates = []
   end
 
+  # number of digits in number plus the line length
+  # or
+  # line length
+  # 114 can can 10-14
+
   def find_symbol_coordinates(lines)
     x = 0
     y = 0
